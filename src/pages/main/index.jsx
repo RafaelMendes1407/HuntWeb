9dc5c34e229210652f8280cfc24e1bfbd9a90ea5
RafaelMendes1407 <rafael_mendes@outlook.com>
2020-04-06 19:59:13 -0300

    Atualização de Desenvolvimento

---
+++ src/pages/main/index.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+import api from '../../services/api.js';
+
+export default class Main extends Component {
+    state = {
+        products: [],
+    }
+
+    componentDidMount(){
+        this.loadProducts();
+    }
+
+    loadProducts = async () => {
+        const res = await api.get('/products');
+        console.log(res);
+    };
+
+    render () {
+        return(
+            <h1>Hello World</h1>
+        );
+    }
+}
